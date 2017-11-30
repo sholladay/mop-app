ad60d78462c060ce76fbc2666e019087413ffb4d
Stan <nedbailov375426@gmail.com>
2017-11-30 05:04:46 -0500

    Output path of failed  projects

--- mop.js
+++ mop.js
@@ -2,11 +2,33 @@
 
 // Our code start here
 
-// const mop = require('mop-cli');
+const mop = require('mop-cli');
 
 const mopButton = document.querySelector('#mop');
+const par = document.querySelector('#output');
+console.log(par);
+mopButton.addEventListener('click', async () => {
+	// console.log(process.versions);
+    // console.log('cwd:', process.cwd());
+    const projects = await mop({
+        rule: {
+            'caret-deps': 'warn'
+        }
+    });
+    const output = projects.map((project) => {
+        let pp = document.createElement('p');
+        return pp.textContent = project.path;
+    });
 
-mopButton.addEventListener('click', () => {
-	console.log(process.versions);
-	console.log('cwd:', process.cwd());
-});
+    const output = projects.map((project) => {
+        let pp = document.createElement('p');
+        return pp.textContent = project.path;
+    });
+
+    console.log(output)
+
+    // output.map(par => {
+
+    // })
+    par.textContent = output
+    });
